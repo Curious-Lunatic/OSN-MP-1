aee8d44b3a267c4e7b99d60ba8acc32fb24bbd13
Soubhagya <soubhagyarbiswal@gmail.com>
2026-08-24 02:22:16 +0530

    makefile+header

---
+++ mini-project1/c-shell/include/shell.h
@@ -0,0 +1,12 @@
+#ifndef shell_h
+#define shell_h
+#include<stdio.h>
+#include<stdlib.h>
+#include<strings.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<sys/select.h>
+#include<sys/types.h>
+#include<pwd.h>
+#include<
+#endif
