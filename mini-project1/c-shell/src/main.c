3d682bd92118cdc79f34ba64094cef8e02e01663
Soubhagya <soubhagyarbiswal@gmail.com>
2026-08-24 10:59:56 +0530

    A2 added

--- mini-project1/c-shell/src/main.c
+++ mini-project1/c-shell/src/main.c
@@ -1,10 +1,24 @@
-#include "../include/shell.h"
+#include "../include/shell.h" // get our shit
 int main(){
     char shome[5000];
+    char input[maxinput]; // given 
     if(getcwd(shome, sizeof(shome)) == NULL){
         return 1;
     }
-    printing(shome);
-    printf("\n");
+    int running=1;
+    while(running){
+        printing(shome);
+        // fgets > scanf bcs does not break on space
+        if (fgets(input, sizeof(input), stdin) == NULL){
+            // failed to read anything so print a new line and break the loop to exit
+            printf("\n");
+            break;
+        }
+        input[strcspn(input, "\n")]='\0';
+        // terminate the line by finding where \n was and continue looping
+        if(strlen(input)==0){
+            continue;
+        }
+    }
 return 0;
 }
