0a44f1c98948721d69fc4a0474e6d9d6257fe897
Soubhagya <soubhagyarbiswal@gmail.com>
2026-08-24 11:05:34 +0530

    commented A1

--- mini-project1/c-shell/src/prompt.c
+++ mini-project1/c-shell/src/prompt.c
@@ -5,23 +5,23 @@ void printing(const char *shome){
     const char* username;
     struct passwd *pwd = getpwuid(getuid());
     if(pwd){
-        username = pwd->pw_name;
+        username = pwd->pw_name; // get username
     }
     else{
-        username = "unknown";
+        username = "unknown"; // doesn't have so put an alias
     }
     if(gethostname(hostname, sizeof(hostname))){
-        strcpy(hostname,"unknown");
+        strcpy(hostname,"unknown"); // get the system's hostname [diff than the username as this was given to the system in the start]
     }
     if(getcwd(cwd,sizeof(cwd))==NULL){
         return;
     }
-    size_t homelength = strlen(shome); // using an unsigned integar
+    size_t homelength = strlen(shome); // using an unsigned integar [typo] to find the size oof the shell's directoryu
     if(strncmp(cwd, shome, homelength) ==0 && (cwd[homelength]=='\0' || cwd[homelength] == '/')){
-        printf("<%s@%s>:~%s ", username, hostname, cwd+homelength); // not a string dumbass
+        printf("<%s@%s>:~%s ", username, hostname, cwd+homelength); // not a string dumbass [ai ts ;-;]
     }
     else{
       printf("<%s@%s:%s> ", username, hostname, cwd);
     }
-    fflush(stdout);
+    fflush(stdout); // flush the output
 }
