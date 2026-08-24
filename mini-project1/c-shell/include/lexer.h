b7a2af133a348471697497e8b85b238ca4ba46f5
Soubhagya <soubhagyarbiswal@gmail.com>
2026-08-24 12:30:38 +0530

    A3 test

---
+++ mini-project1/c-shell/include/lexer.h
@@ -0,0 +1,21 @@
+#ifndef lexer_h
+#define lexer_h // bruh i only do typos ;-;
+typedef enum{
+    token_pipe,
+    token_amp,
+    token_semi,
+    token_lt,
+    token_gt,
+    token_gtgt,
+    token_word,
+    token_fragment,
+    token_dq,
+    token_sq
+} tokentype;
+typedef struct token
+{
+    tokentype type;
+    char value[5000];
+}token;
+int maketoken(char *input, token *token, int *tok_count);
+#endif
---
+++ mini-project1/c-shell/src/lexer.c
@@ -0,0 +1,78 @@
+#include "../include/shell.h" // get our shit
+#include "../include/lexer.h" // get our other shit
+int maketoken(char *input, token *tokens, int *tok_count){
+        *tok_count = 0; // for the lexar
+        int state = 0; // for the grammer
+        char word[1024]; // max given
+        int index = 0;
+    
+    for(int i=0; input[i]!='\0'; i++){
+        char c = input[i]; // go char by char
+        if (state==0){
+            if (c=='\\'){
+                if(input[i+1]=='\0'){
+                    printf("cshell: invalid syntax"); // given if there is an invalid command
+                    return 0;
+                }
+            i++;
+            word[index++] = input[i];
+            }
+        else if(c=='\'') state = 1;
+        else if(c=='"') state = 2;
+        else if (c=='\n' || c=='\t' || c==' '){
+            if(index>0){
+                word[index] = '\0';
+                tokens[*tok_count].type = token_word;
+                strcpy(tokens[*tok_count].value, word);
+                (*tok_count)++;
+                index=0;
+            }
+        }
+        else if (c=='|' || c=='&' || c==';' || c=='<' || c=='>'){
+            if(index>0){
+                word[index] = '\0';
+                tokens[*tok_count].type = token_word;
+                strcpy(tokens[*tok_count].value, word);
+                (*tok_count)++;
+                index=0;
+            }
+        if (c=='>' && input[i+1] == '>'){
+            tokens[*tok_count].type = token_gtgt; // >>
+            i++;
+        }
+        else if (c=='>') tokens[*tok_count++].type = token_gt;
+        else if (c=='<') tokens[*tok_count++].type = token_lt;
+        else if (c=='|') tokens[*tok_count++].type = token_pipe;
+        else if (c=='&') tokens[*tok_count++].type = token_amp;
+        else if (c==';') tokens[*tok_count++].type = token_semi;
+        }
+        else{
+            word[index++]=c;
+        }
+    }
+        else if (state==1){
+            if (c=='\'') state = 0;
+            else word[index++]=c;
+        }
+        else if (state==2){
+            if (c=='\\' && (input[i+1]=='"' || input[i+1]=='\\')){
+                i++;
+                word[index++] = input[i];
+            }
+            else if (c =='"') state =0;
+            else word[index++] = c;
+            }
+        }
+     if(index>0){
+                word[index] = '\0';
+                tokens[*tok_count].type = token_word;
+                strcpy(tokens[*tok_count].value, word);
+                (*tok_count)++;
+                index=0;
+            }
+    if(state!=0){
+        printf("cshell: invalid syntax\n");
+        return 0;
+    }  
+    return 1; 
+}    
